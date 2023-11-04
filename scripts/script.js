09bfa406c51414832702c2bc5e14b04e5ddd6243
Matias Ariel Coco <cocomatias6@gmail.com>
2023-11-04 14:29:13 -0300

    update

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+    $(".menu-toggle").click(function() {
+        $(".nav-list").slideToggle();
+    });
+
+
+    $(".nav-link").on("mouseenter", function() {
+        $(this).css("background-color", "#ed8634");
+    });
+
+    $(".nav-link").on("mouseleave", function() {
+        $(this).css("background-color", "#114380");
+    });
+
+});
