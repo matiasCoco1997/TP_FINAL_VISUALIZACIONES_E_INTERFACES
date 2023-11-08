50c4cb36affa9eb903743e36a86d140ae3c8b3fe
Facu Ortega <fortega@finnegans.com.ar>
2023-11-07 23:10:59 -0300

    se agrega loading, validaciones y efecto en css loading

--- scripts/login.js
+++ scripts/login.js
@@ -3,20 +3,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const emailInput = document.getElementById("email");
     const passwordInput = document.getElementById("password");
-    
+    const errorMessage = document.getElementById("password-email-error");
+
     const form = document.querySelector(".loginContainerForm");
     
     form.addEventListener("submit", function(event) {
         event.preventDefault();
-        let hasError = false;
-        
-        console.log('hola')
+
+        let email = false
+
 
         if (emailInput.value.trim() === "") {
         emailInput.classList.add("error");
         document.getElementById("email-error").style.display = "block";
         hasError = true;
         } else {
+        email = true
         emailInput.classList.remove("error");
         document.getElementById("email-error").style.display = "none"; 
         }
@@ -29,10 +31,23 @@ document.addEventListener("DOMContentLoaded", function() {
         passwordInput.classList.remove("error");
         document.getElementById("password-error").style.display = "none";
         }
-    
-        if (hasError) {
-        event.preventDefault();
+
+        if (passwordInput.value.trim() !== "1234") {
+        passwordInput.classList.add("error");
+        errorMessage.style.display = "block";
+        hasError = true;
+        } else {
+        passwordInput.classList.remove("error");
+        errorMessage.style.display = "none";
         }
+
+        if  (email) {
+        document.querySelector(".loading").style.display = "flex";
+        setTimeout(function() {
+            document.querySelector(".loading").style.display = "none";
+            }, 2000)
+        }
+
     });
 });
 
