420805902ff862456347648eca9b9133301969e6
Matias Ariel Coco <cocomatias6@gmail.com>
2023-11-09 10:05:35 -0300

    update mostrar errores cuando me registro

--- scripts/registrarse.js
+++ scripts/registrarse.js
@@ -2,13 +2,9 @@
 $(document).ready(function() {
 
     const nombreInput = $("#nombre");
-
     const dniInput = $("#dni");
-
     const emailInput = $("#email");
-
     const passwordInput = $("#password");
-
     const form = $(".loginContainerForm");
 
     form.submit(function(event) {
@@ -19,37 +15,45 @@ $(document).ready(function() {
         if (nombreInput.val().trim() === "") {
             nombreInput.addClass("error");
             $("#nombre-error").show();
+            $("#nombreContainer").addClass("errorInput");
             hasError = true;
         } else {
             nombreInput.removeClass("error");
             $("#nombre-error").hide();
+            $("#nombreContainer").removeClass("errorInput");
         }
 
         if (dniInput.val().trim() === "") {
             dniInput.addClass("error");
             $("#dni-error").show();
+            $("#dniContainer").addClass("errorInput");
             hasError = true;
         } else {
             dniInput.removeClass("error");
             $("#dni-error").hide();
+            $("#dniContainer").removeClass("errorInput");
         }
 
         if (emailInput.val().trim() === "") {
             emailInput.addClass("error");
             $("#email-error").show();
+            $("#emailContainer").addClass("errorInput");
             hasError = true;
         } else {
             emailInput.removeClass("error");
             $("#email-error").hide();
+            $("#emailContainer").removeClass("errorInput");
         }
 
         if (passwordInput.val().trim() === "") {
             passwordInput.addClass("error");
             $("#password-error").show();
+            $("#passwordContainer").addClass("errorInput");
             hasError = true;
         } else {
             passwordInput.removeClass("error");
             $("#password-error").hide();
+            $("#passwordContainer").removeClass("errorInput");
         }
 
         if (hasError) {
