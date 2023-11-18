3ccb864edac27df158a29676de198ff5041e1aa6
Facu Ortega <fortega@finnegans.com.ar>
2023-11-18 20:30:49 -0300

    se cargan mensajes de errores y se agrega horario

--- scripts/form.js
+++ scripts/form.js
@@ -13,7 +13,20 @@ document.addEventListener('DOMContentLoaded', function () {
     var ApellidoVacio = false;
     var localidadVacia = false;
 
-    
+    var diasContainer = document.querySelector('.dias-container');
+    var mesActualElement = document.getElementById('mesActual');
+    var fechaActual = new Date();
+    var mesActual = fechaActual.getMonth();
+    var anioActual = fechaActual.getFullYear();
+
+    var flagDate = false
+
+    var flagDateInvalid = false
+
+    var selectDate = document.getElementById('Date');
+    var errorInputEmpty = document.querySelector('.errorInputEmpty');
+
+
 
     succesBTN.addEventListener('click', function () {
 
@@ -93,6 +106,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.querySelector('.calendarContainer').style.display = 'flex';
                 pasoActual = 2;
             } else if (pasoActual === 2) {
+                if ( flagDateInvalid = false) return
+                if(!flagDate){ 
+                    errorInputEmpty.style.display = selectDate.value === '0' ? 'block' : 'none';
+                    return
+                }
+
+
                 dangerBTN.style.display="flex"
                 document.querySelector('.btn-home').style.display = 'none';
                 var progressBar = document.querySelector(".progress-bar")
@@ -148,6 +168,160 @@ document.addEventListener('DOMContentLoaded', function () {
             $(location).attr('href', "./home.html");
         }, 1000);
     })
+
+    // -------------------------Calendario---------------- //
+
+
+
+    selectDate.addEventListener('change', function () {
+
+        if( selectDate.value === '0'){
+            errorInputEmpty.style.display = 'block'
+        }
+        else{
+            errorInputEmpty.style.display = 'none'
+            flagDate = true
+        }
+    });
+    
+    function mostrarCalendario(mes, anio) {
+        // Limpiar el contenedor antes de mostrar el nuevo mes
+        diasContainer.innerHTML = '';
+
+        var diasSemana = ['D', 'L', 'M', 'M', 'J', 'V', 'S'];
+
+        // Agregar los nombres de los días de la semana
+        diasSemana.forEach(function (nombreDia) {
+            var diaSemana = document.createElement('div');
+            diaSemana.classList.add('dia', 'nombre-dia');
+            diaSemana.textContent = nombreDia;
+            diasContainer.appendChild(diaSemana);
+        });
+
+        // Obtener el primer día del mes y el último día del mes
+        var primerDiaMes = new Date(anio, mes, 1).getDay();
+        var ultimoDiaMes = new Date(anio, mes + 1, 0).getDate();
+
+        // Calcular el número total de días que necesitamos mostrar (incluyendo los días del mes anterior y siguiente)
+        var diasTotales = Math.ceil((primerDiaMes + ultimoDiaMes) / 7) * 7;
+
+        // Calcular cuántos días del mes anterior necesitamos mostrar al principio del calendario
+        var diasMesAnterior = primerDiaMes > 0 ? primerDiaMes : 7;
+
+        // Agregar días del mes anterior al principio del calendario
+        for (var i = diasMesAnterior - 1; i >= 0; i--) {
+            var diaAnterior = document.createElement('div');
+            diaAnterior.classList.add('dia', 'mes-anterior', 'cuadrado-blanco');
+            diasContainer.appendChild(diaAnterior);
+        }
+
+        // Agregar los elementos del calendario
+
+        for (var i = 1; i <= ultimoDiaMes; i++) {
+            var dia = document.createElement('div');
+            var DateInput = document.querySelector('#campoInputDate')
+            var DateError = document.querySelector('.errorDay')
+            
+            dia.classList.add('dia');
+            dia.classList.add(i)
+            dia.textContent = i;
+
+            // Marcar algunos días en verde (puedes personalizar esta lógica según tus necesidades)
+            if (i % 2 === 0) {
+                dia.classList.add('verde');
+                (function (diaActual) {
+                    flagDate = false
+                    dia.addEventListener('click', function() {
+                        var diaPintado = document.querySelector(".azul");
+                        var diaClick =  document.getElementsByClassName(diaActual)[0]
+                        if (diaPintado) { 
+                            diaPintado.classList.remove("azul");
+                        }
+                        diaClick.classList.add("azul");
+                        DateInput.style.display = "flex"
+                        DateError.style.display = "none"
+                        flagDateInvalid = false
+                    });
+                })(i);
+            }else{
+                (function (diaActual) {
+                    flagDate = false
+                    flagDateInvalid = true
+                    dia.addEventListener('click', function() {
+                        var diaPintado = document.querySelector(".azul");
+                        if (diaPintado) { 
+                            diaPintado.classList.remove("azul");
+                        }
+                        DateInput.style.display = "none"
+                        DateError.style.display = "block"
+                        errorInputEmpty.style.display = "none"
+                    });
+                   
+                })(i);
+            }
+
+            diasContainer.appendChild(dia);
+        }
+
+        // Calcular cuántos días del mes siguiente necesitamos mostrar al final del calendario
+        var diasMesSiguiente = diasTotales - (diasMesAnterior + ultimoDiaMes);
+
+        // Agregar días del mes siguiente al final del calendario
+        for (var i = 1; i <= diasMesSiguiente; i++) {
+            var diaSiguiente = document.createElement('div');
+            diaSiguiente.classList.add('dia', 'mes-siguiente', 'cuadrado-blanco');
+            diasContainer.appendChild(diaSiguiente);
+        }
+
+        // Actualizar el mes actual en el elemento HTML
+        mesActualElement.textContent = obtenerNombreMes(mes) + ' ' + anio;
+    }
+
+
+    document.getElementById('prevButton').addEventListener('click', function () {
+        cambiarMes(-1);
+    });
+
+    document.getElementById('nextButton').addEventListener('click', function () {
+        cambiarMes(1);
+    });
+
+
+    function obtenerNombreMes(numeroMes) {
+        var nombresMeses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        return nombresMeses[numeroMes];
+    }
+
+
+    function cambiarMes(direccion) {
+        // Obtener la fecha límite (noviembre de 2023)
+        var fechaLimite = new Date("November 1, 2023");
+    
+        // Calcular la nueva fecha antes de cambiar el mes
+        var nuevaFecha = new Date(anioActual, mesActual + direccion, 1);
+    
+        // Comparar con la fecha límite
+        if (direccion < 0 && nuevaFecha < fechaLimite) {
+            // Si se intenta ir antes de noviembre de 2023, no hacer nada
+            return;
+        }
+    
+        // Cambiar el mes según la dirección
+        mesActual += direccion;
+    
+        if (mesActual < 0) {
+            mesActual = 11;
+            anioActual--;
+        } else if (mesActual > 11) {
+            mesActual = 0;
+            anioActual++;
+        }
+    
+        mostrarCalendario(mesActual, anioActual);
+    }
+
+
+    mostrarCalendario(mesActual, anioActual);
 
 
 });
