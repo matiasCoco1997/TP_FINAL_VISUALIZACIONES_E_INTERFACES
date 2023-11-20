3fb7a6d19d253d9b7cfe8b1fa3047a4c952c755c
Matias Ariel Coco <cocomatias6@gmail.com>
2023-11-20 17:03:09 -0300

    update registrarse con pop up

--- scripts/registrarse.js
+++ scripts/registrarse.js
@@ -15,36 +15,44 @@ $(document).ready(function() {
         if (nombreInput.val().trim() === "") {
             nombreInput.addClass("error");
             $("#nombreContainer").addClass("errorBorderBottom");
+            $("#usuarioIcon").addClass("errorIcon");
             hasError = true;
         } else {
             nombreInput.removeClass("error");
             $("#nombreContainer").removeClass("errorBorderBottom");
+            $("#usuarioIcon").removeClass("errorIcon");
         }
 
         if (dniInput.val().trim() === "") {
             dniInput.addClass("error");
             $("#dniContainer").addClass("errorBorderBottom");
+            $("#dniIcon").addClass("errorIcon");
             hasError = true;
         } else {
             dniInput.removeClass("error");
             $("#dniContainer").removeClass("errorBorderBottom");
+            $("#dniIcon").removeClass("errorIcon");
         }
 
         if (emailInput.val().trim() === "") {
             emailInput.addClass("error");
             $("#emailContainer").addClass("errorBorderBottom");
+            $("#emailIcon").addClass("errorIcon");
             hasError = true;
         } else {
             emailInput.removeClass("error");
+            $("#emailIcon").removeClass("errorIcon");
             $("#emailContainer").removeClass("errorBorderBottom");
         }
 
         if (passwordInput.val().trim() === "") {
             passwordInput.addClass("error");
+            $("#keyIcon").addClass("errorIcon");
             $("#passwordContainer").addClass("errorBorderBottom");
             hasError = true;
         } else {
             passwordInput.removeClass("error");
+            $("#keyIcon").removeClass("errorIcon");
             $("#passwordContainer").removeClass("errorBorderBottom");
         }
 
@@ -56,11 +64,25 @@ $(document).ready(function() {
         } else{
             $("#campos-error").hide();
             $("#campos-error").removeClass("errorInput");
-            $(location).attr('href', "./inciar_sesion.html");
+            $("#overlay").show();
+            $("#popup").show();
+            
         }
-
     });
+
+    document.getElementById("btn_redireccion").addEventListener("click", function(){
+        ocultarPopup();
+    })
+
+    function ocultarPopup() {
+        $(".overlay").hide();
+        $(".popup").hide();
+        $(location).attr('href', "./inciar_sesion.html");
+    }
+
 });
+
+
 
 
 
